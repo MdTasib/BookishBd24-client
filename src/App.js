4b4cae2765838c48e269a0f7cae8ea40a88c7d53
MdTasib <ohidulalam442@gmail.com>
2022-11-16 12:07:18 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/ui/Footer";
 import Navbar from "./components/ui/Navbar";
 
 function App() {
 	return (
 		<Router>
 			<Navbar />
+			<Footer />
 		</Router>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,19 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#F63E7B",
+					secondary: "#FFF8F5",
+					success: "#e2fcf2",
+					accent: "#F4F7FC",
+					neutral: "#3d4451",
+					"base-100": "#ffffff",
+				},
+			},
+		],
+	},
 	plugins: [require("daisyui")],
 };
