867061c63be28de6d7738a3c3bf2fea0a288a5c8
MdTasib <ohidulalam442@gmail.com>
2022-11-18 15:45:32 +0600

    dashboard desing complete

--- src/components/dashboard/AddQuestion.js
+++ src/components/dashboard/AddQuestion.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const AddQuestion = () => {
-	return <div>AddQuestion</div>;
+	const { register, handleSubmit, reset } = useForm();
+
+	const onSubmit = async data => {
+		fetch(`http://localhost:5000/product`, {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify({}),
+		})
+			.then(res => res.json())
+			.then(data => {
+				reset();
+				Swal.fire({
+					position: "top-center",
+					icon: "success",
+					title: "Successfully upload a new product",
+					showConfirmButton: false,
+					timer: 1500,
+				});
+			});
+	};
+
+	return (
+		<div className='hero'>
+			<div className='hero-content w-full'>
+				<div className='card w-full shadow-2xl bg-base-100'>
+					<form onSubmit={handleSubmit(onSubmit)} className='card-body'>
+						<div className='form-control'>
+							<div className=''>
+								<label className='label'>
+									<span className='label-text'>Question?</span>
+								</label>
+								<input
+									{...register("name", { required: true })}
+									type='text'
+									placeholder='Your Question'
+									className='input input-bordered input-primary w-full'
+								/>
+							</div>
+
+							<div className='form-control'>
+								<label className='label'>
+									<span className='label-text'>Question Answer</span>
+								</label>
+								<textarea
+									{...register("description", { required: true })}
+									className='textarea textarea-primary w-full'
+									placeholder='Your Answer'></textarea>
+							</div>
+						</div>
+
+						<div className='form-control mt-6'>
+							<button className='btn btn-primary'>ADD QUESTION</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AddQuestion;
--- src/components/dashboard/DashboardIntro.js
+++ src/components/dashboard/DashboardIntro.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DashboardIntro = () => {
-	return <div>DashboardIntro</div>;
+	return (
+		<div className='text-center flex items-center justify-center h-full'>
+			<h2 className='text-4xl text-primary font-bold'>Welcome to Dashboard</h2>
+		</div>
+	);
 };
 
 export default DashboardIntro;
