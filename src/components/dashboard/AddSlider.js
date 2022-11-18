8242e5417eefe17b068ea5d82152bb65cf721006
MdTasib <ohidulalam442@gmail.com>
2022-11-18 15:12:42 +0600

    add slider

--- src/components/dashboard/AddSlider.js
+++ src/components/dashboard/AddSlider.js
@@ -1,7 +1,41 @@
 import React from "react";
 
 const AddSlider = () => {
-	return <div>AddSlider</div>;
+	return (
+		<>
+			<div class='border-box'>
+				<div class='flex justify-center w-full mx-auto sm:max-w-lg'>
+					<div class='flex flex-col items-center justify-center w-full h-auto my-16 bg-white sm:w-3/4 sm:rounded-lg sm:shadow-xl'>
+						<div class='mt-10 mb-10 text-center'>
+							<h2 class='text-2xl font-semibold mb-2'>Upload your files</h2>
+							<p class='text-xs text-gray-500'>
+								File should be of format .jpg, .png, .jpeg
+							</p>
+						</div>
+						<form
+							action='#'
+							class='relative w-4/5 h-32 max-w-xs mb-10 bg-white bg-accent rounded-lg shadow-inner'>
+							<input type='file' id='file-upload' class='hidden' />
+							<label
+								for='file-upload'
+								class='z-20 flex flex-col-reverse items-center justify-center w-full h-full cursor-pointer'>
+								<p class='z-10 text-xs font-light text-center text-gray-500'>
+									Select your files here
+								</p>
+								<svg
+									class='z-10 w-8 h-8 text-primary'
+									fill='currentColor'
+									viewBox='0 0 20 20'
+									xmlns='http://www.w3.org/2000/svg'>
+									<path d='M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z'></path>
+								</svg>
+							</label>
+						</form>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AddSlider;
