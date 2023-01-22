07f7346e7efb1d72a99d22a96d729f17f7529d25
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-23 01:43:27 +0600

    profile updated

--- src/components/dashboard/profile/MyProfile.js
+++ src/components/dashboard/profile/MyProfile.js
@@ -16,7 +16,7 @@ const MyProfile = () => {
         <div className='h-[100vh]'>
             <div>
                 <div className='w-[70%] mx-auto'>
-                    <div className='bg-gray-200 rounded-md mt-12 py-4 px-12'>
+                    <div className='bg-gray-200 rounded-md mt-12 py-4 px-4 lg:px-12 md:px-12'>
                         <div className='flex justify-between'>
                             <h2 className='mb-5 text-xl font-bold'>My Profile</h2>
                             <div onClick={() => updateUserProfile()}  className='flex items-center text-primary font-bold'>
@@ -24,12 +24,12 @@ const MyProfile = () => {
                                 <p className='ml-1'>Edit</p>
                             </div>
                         </div>
-                        <div className='flex gap-8 '>
+                        <div className='lg:flex md:flex gap-8 '>
                             <div className=''>
                                 <img className='h-24 w-24 rounded-full' src={bookimg} alt="" />
                                 <button onClick={() => updateUserProfile()} className='px-2 py-2 bg-primary rounded-md text-white mt-4'>Edit profile</button>
                             </div>
-                            <div className='mt-2 ml-8 font-bold'>
+                            <div className='mt-2 lg:ml-8 md:ml-8 font-bold'>
                                 <small>Name:</small>
                                 <p className='mb-2'>Ridima Rahman Mou</p>
                                 <small>Email:</small>
