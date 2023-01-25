527ed603835545714be799c85a8041a29d15a8a6
MdTasib <ohidulalam442@gmail.com>
2023-01-25 11:02:55 +0600

    subject get by redux

--- src/components/SectionBooks/SectionBooks.js
+++ src/components/SectionBooks/SectionBooks.js
@@ -13,7 +13,6 @@ const SectionBooks = ({ title, filters }) => {
 		isError,
 		error,
 	} = useGetBooksQuery({ ...filters });
-	console.log(filters);
 
 	const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	const responsive = {
