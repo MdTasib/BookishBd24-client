d897535bcac46b9cc42dc0b42071f264a2010faf
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-02 10:40:27 +0600

    education section added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import AdsGrid from "../../components/Ads/AdsGrid";
 import SectionBooks from "../../components/SectionBooks/SectionBooks";
 import ImageSlider from "../../components/Slider/Slider";
+import Education from "./education/Education";
 import PopularAuthor from "./popularAuthor/PopularAuthor";
 
 function Home() {
 	return (
 		<div>
 			<ImageSlider />
+			<Education />
 			<SectionBooks 
 				filters={{ category: "বইমেলা" }}
 				title='ইসলামী সাহিত্য, গল্প-উপন্যাস এবং সফরনামা'
