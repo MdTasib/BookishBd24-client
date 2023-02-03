dde8848959d8666234d952131226b7ce71638697
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-03 11:24:41 +0600

    home searchbar added

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,4 @@
-import { data } from "autoprefixer";
 import React from "react";
-import { useEffect } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
@@ -11,22 +9,17 @@ import MenuBar from "./MenuBar";
 
 const Navbar = () => {
 	const navigate = useNavigate();
-	const [value, setValue] = useState("");
-	const [cursorPosition, setCursorPosition] = useState();
-	console.log(cursorPosition);
+	const inputRef = useRef(null)
 	const [searchListVisible, setSearchListVisible] = useState(true);
-	const handleSearchResultClick = (data) => {
-		// setSearchListVisible(!false);
-		// navigate to other page
-		navigate(`book/${data._id}`);
-		setValue("");
-		console.log("cheak Value", value);
-	};
+
+	const reSeatInput = () =>{
+		setSearchData([]);
+		inputRef.current.value = "";
+		// console.log(inputRef.current.value);
+	}
 	
-
-
 	const [searchData, setSearchData] = useState([]);
-	console.log(searchData);
+	// console.log(searchData);
 
 
 	const { data: books, isLoading, isError } = useGetBooksQuery();
@@ -50,10 +43,12 @@ const Navbar = () => {
 		}
 	}
 
+	
 
 
 	const handleNavigate = () => {
 		navigate("/cart");
+		
 	};
 	const menuItems = (
 		<>
@@ -135,6 +130,7 @@ const Navbar = () => {
 							<div className='flex border border-primary border-2'>
 
 								<input
+								 ref = {inputRef}
 									type='text'
 									className='px-4 py-2 input-sm w-24 md:w-80 input-primary'
 									placeholder='বইয়ের নাম ও লেখক দিয়ে অনুসন্ধান করুন'
@@ -157,7 +153,7 @@ const Navbar = () => {
 									{searchData.length ?
 										<ul className="top-12 w-[376px] h-auto px-4 bg-gray-300 border-2 z-10 border-primary mr-2">
 											{searchData.map(data =>
-												<li onClick={() => handleSearchResultClick(data)} className="cursor-pointer">{data.name}</li>)
+												<li onClick={() => reSeatInput()} className="cursor-pointer"><NavLink to={`book/${data._id}`}>{data.name}</NavLink></li>)
 											}
 
 										</ul>
