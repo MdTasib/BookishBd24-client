2ab7f2659e94a31cb58122b6e0388737ab52d296
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-05 22:21:44 +0600

    general book slider

--- src/pages/generalBook/GeneralBook.js
+++ src/pages/generalBook/GeneralBook.js
@@ -52,6 +52,25 @@ const GeneralBook = () => {
 		));
 	}
 
+	// GENERAL BOOK SLIDER
+	const sliders = [
+		{
+			id: 1,
+			image:
+				"https://wafilife-media.wafilife.com/uploads/2020/01/general-books-self-development-book.jpg",
+		},
+		{
+			id: 2,
+			image:
+				"https://wafilife-media.wafilife.com/uploads/2020/01/programming.jpg",
+		},
+		{
+			id: 3,
+			image:
+				"https://wafilife-media.wafilife.com/uploads/2021/03/aslaf_desktop-1.jpg",
+		}
+	];
+
 	const settings = {
 		arrows: false,
 		autoplay: true,
