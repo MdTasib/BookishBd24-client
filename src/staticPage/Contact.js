cbc3513d1aba70aee4099c58e13b9e04af2d0305
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-12 14:57:33 +0600

    file created

---
+++ src/staticPage/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../components/ui/Container';
+
+const Contact = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
+
+export default Contact;
