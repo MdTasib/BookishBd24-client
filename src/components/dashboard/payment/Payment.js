afc0f5a132b9ccd383ac82167afcfdd996b2510b
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-02-28 19:37:56 +0600

    payment seystem

--- src/components/dashboard/payment/Payment.js
+++ src/components/dashboard/payment/Payment.js
@@ -7,15 +7,26 @@ const stripePromise = loadStripe('pk_test_51L0gMADuiIiaFlXNz9N5k6HXIPhnvURpmXcZe
 
 const Payment = () => {
     return (
-        <div className='bg-gray-100 pb-12 h-screen'>
-            <div className='text-center text-lg font-roboto py-5'>
-                <h3>PAY FOR YOUR Orders - <span className='text-[#1E88E5]'>Book Name</span></h3>
-                <p className=''>Please Pay : <span className='text-red-600'>$120</span></p>
+        <div className='bg-gray-100 pb-12 h-screen grid grid-cols-2 gap-4'>
+            <div>
+                <div className='text-center text-lg font-roboto py-2 w-96 mx-auto mt-5 bg-white mb-5 rounded-lg'>
+                    <h3>PAY FOR YOUR Orders - <span className='text-[#1E88E5]'>Book Name</span></h3>
+                    <p className=''>Please Pay : <span className='text-red-600'>$120</span></p>
+                </div>
+                <div className='bg-white rounded-2xl shadow-2xl w-96 mx-auto p-8 '>
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm />
+                    </Elements>
+                </div>
             </div>
-            <div className='bg-white rounded-2xl shadow-2xl w-96 mx-auto p-8 '>
-                <Elements stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
+            <div>
+                <div className='bg-white mt-5 w-64 py-4 rounded-md shadow-lg leading-10'>
+                    <h4 className='ml-4 text-green-700 font-bold'>পেমেন্ট কিভাবে সম্পন্ন করবেন?</h4>
+                    <p className='ml-4'>১. আপনার কার্ড নাম্বারটি দিন</p>
+                    <p className='ml-4'>২. এক্সপায়ারেশন ডেট লিখুন</p>
+                    <p className='ml-4'>৩. CVC লিখুন (তিন সংখ্যার ডিজিট)</p>
+                    <p className='ml-4'>৪. এবং সবশেষে জিপ কোড দিন</p>
+                </div>
             </div>
         </div>
     );
